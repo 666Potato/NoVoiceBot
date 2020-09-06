53ee06a991b344e90dfc1832b3c6c7ce093845d0
mercury406 <69970854+mercury406@users.noreply.github.com>
2020-09-06 18:43:37 +0500

    Update no_voice_bot.py

--- no_voice_bot.py
+++ no_voice_bot.py
@@ -1,35 +1,35 @@
 from telegram.ext import Updater, MessageHandler, Filters
 from wit import Wit
 import ffmpeg
+import os
 
 CLIENT = Wit('CN76OWR2WNWJPNRKRRPUOTDZIHHCQ6L3')
 
-
 def echo(update, context):
     """Echo the user message."""
+    print(update)
     audio_oga = context.bot.get_file(update.message.voice).download()
     stream = ffmpeg.input(audio_oga)
     stream = ffmpeg.output(stream, 'audio.wav')
     ffmpeg.run(stream)
+    if os.path.exists(audio_oga):
+        os.remove(audio_oga)
     audio = open('audio.wav', 'rb')
     text = CLIENT.post_speech(audio)
+    if os.path.exists("audio.wav"):
+        os.remove("audio.wav")
     final_text_json = text['_text']
-    update.message.reply_text(final_text_json)
-
+    update.message.reply_text(final_text_json, reply_to_message_id = update.message.message_id)
 
 def main():
     """Start the bot."""
     updater = Updater("1260098169:AAFB855CpwQwcSn4c9fjTKjkNIcCtUX8IYk", use_context=True)
-
     dp = updater.dispatcher
-
-    dp.add_handler(MessageHandler(Filters.all, echo))
-
+    dp.add_handler(MessageHandler(Filters.voice, echo))
     # Start the Bot
     updater.start_polling()
     # Wait for other requests
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
